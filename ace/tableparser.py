abda9781ee21f202ab97e16e124884d32b3798e1
Alejandro de la Vega <aleph4@gmail.com>
2023-03-27 13:02:26 -0500

    Get rid of periods

--- ace/tableparser.py
+++ ace/tableparser.py
@@ -216,6 +216,9 @@ def parse_table(data):
         r = data[i]
         found_xyz = regex.search('\d+.*\d+.*\d+', '/'.join(r))  # use this later
         for j, val in enumerate(r):
+            if val[-1] == '.':
+                val = val[:-1]
+                
             val = val.strip()
             # If a value is provided and the cell isn't an overflow cell (i.e., '@@'), and
             # there is no current label assigned to this column...
