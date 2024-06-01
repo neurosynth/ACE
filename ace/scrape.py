a800c64f6a6b737fac2da38f1b4f2d59cea6ee2e
James Kent <jamesdkent21@gmail.com>
2024-05-31 20:18:45 -0500

    add another failure to scrape status

--- ace/scrape.py
+++ ace/scrape.py
@@ -275,7 +275,8 @@ def _validate_scrape(html):
     'Enable JavaScript and cookies to continue',
     'There was a problem providing the content you requested',
     '<title>Redirecting</title>',
-    '<title>Page not available - PMC</title>']
+    '<title>Page not available - PMC</title>',
+    'Your request cannot be processed at this time. Please try again later']
 
     for pattern in patterns:
         if pattern in html:
